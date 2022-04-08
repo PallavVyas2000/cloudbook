57c2e3504e334c75982e34afab64c46273a9ab7f
Pallav Vyas <pallavvyas2000@gmail.com>
2022-04-09 04:26:40 +0530

    Adding Notes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
-
+var fetchUser = require('../middleware/fetchUser');
 const JWT_Secret = "mysecretstring";
 
-// Create a User using: POST "/api/auth/createuser". No login required
+// Route 1: Create a User using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
     body('email', 'Enter a valid E-mail address').isEmail(),
     body('name', 'Minimum length of name should be 3 characters').isLength({ min: 3 }),
@@ -24,8 +24,10 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists." });
         }
+        // Adding salt and creating hash of the password
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(req.body.password, salt);
+        // Creating user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
@@ -44,7 +46,7 @@ router.post('/createuser', [
     }
 })
 
-// Authenticate a User using: POST "/api/auth/login". No login required
+// Route 2: Authenticate a User using: POST "/api/auth/login". No login required
 router.post('/login', [
     body('email', 'Enter a valid E-mail address').isEmail(),
     body('password', 'Password cannot be blank.').exists()
@@ -80,5 +82,18 @@ router.post('/login', [
         res.status(500).send("Some error occured");
     }
 
+})
+
+// Route 3: Get logged in user details using: POST "/api/auth/getuser". Login required
+router.post('/getuser', fetchUser, async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,7 +1,41 @@
 const express = require('express');
+const fetchUser = require('../middleware/fetchUser');
+const Note = require('../models/Note');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
+
+// ROUTE 1: Get All the Notes using: GET "/api/auth/fetchallnotes". Login required
+router.get('/fetchallnotes', fetchUser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured");
+    }
 })
 
+// ROUTE 2: Add a new note using: POST "/api/auth/addnote". Login required
+router.post('/addnote', fetchUser, [
+    body('title', 'Enter a valid Title').isLength({ min: 1 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        // If there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Some error occured");
+    }
+})
 module.exports = router;
