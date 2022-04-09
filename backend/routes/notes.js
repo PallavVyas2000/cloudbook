276d814f80ca7580f3075337fe9ee20aa160ab3c
Pallav Vyas <pallavvyas2000@gmail.com>
2022-04-09 21:33:41 +0530

    Update Notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 
-// ROUTE 1: Get All the Notes using: GET "/api/auth/fetchallnotes". Login required
+// ROUTE 1: Get All the Notes using: GET "/api/notes/fetchallnotes". Login required
 router.get('/fetchallnotes', fetchUser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id })
@@ -16,7 +16,7 @@ router.get('/fetchallnotes', fetchUser, async (req, res) => {
     }
 })
 
-// ROUTE 2: Add a new note using: POST "/api/auth/addnote". Login required
+// ROUTE 2: Add a new note using: POST "/api/notes/addnote". Login required
 router.post('/addnote', fetchUser, [
     body('title', 'Enter a valid Title').isLength({ min: 1 }),
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
@@ -37,5 +37,26 @@ router.post('/addnote', fetchUser, [
         console.log(error.message);
         res.status(500).send("Some error occured");
     }
+})
+
+// ROUTE 3: Update an existing note using: PUT "/api/notes/updatenote/:id". Login required
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+    const {title, description, tag} = req.body;
+    // Create a newNote object
+    const newNote = {}
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    //Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
 })
 module.exports = router;
