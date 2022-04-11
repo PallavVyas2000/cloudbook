6bc10a69301c550a5c50c36f690ba37cc86f6644
Pallav Vyas <pallavvyas2000@gmail.com>
2022-04-11 21:35:20 +0530

    Initial Structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import logo from '../logo512.png';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <img src={logo} alt="" width="40" height="40" className="d-inline-block align-text-center mx-3" />
+                    <strong>Daily Digest</strong>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
